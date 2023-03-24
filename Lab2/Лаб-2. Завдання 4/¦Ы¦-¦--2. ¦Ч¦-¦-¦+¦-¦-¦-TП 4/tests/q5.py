630bde24b21765c03a2647038c89e69f3296b5d9
zhonchuk <zhonchuk.business@gmail.com>
2023-03-24 20:06:34 +0200

    Added Lab2 Task 1-5

---
+++ Lab2/Лаб-2. Завдання 4/¦Ы¦-¦--2. ¦Ч¦-¦-¦+¦-¦-¦-TП 4/tests/q5.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q5',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(set_c, set)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(set_c) == 2\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> set_c == {8, 14}\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ Lab2/Лаб-2. Завдання 4/¦Ы¦-¦--2. ¦Ч¦-¦-¦+¦-¦-¦-TП 4/tests/q6.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q6', 'points': 1, 'suites': [{'cases': [{'code': '>>> kol_uniq_symb == 26\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
